a2461a17222d8a19d3c18e38a1b982611c99d3df
Jaime Burbano <burbanojaime98@gmail.com>
2020-05-20 23:30:11 -0500

    svelte

---
+++ src/components/TimeLine.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { onMount } from 'svelte'
+
+    import Card from './Card'
+
+    export let post = []
+
+</script>
+
+<style>
+    .TimeLine {
+        padding: 4em 0 0 0;
+    }
+</style>
+
+<div class="TimeLine">
+    <div class="TimeLine-container">
+    {#each post as item}
+         <Card {...item}/>
+
+    {:else}
+
+    {/each}
+        <p>Loading...</p>
+    </div>
+</div>
